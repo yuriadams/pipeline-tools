13ac6534ac9e70f6ce29523e13b0c51239479442
yuriadams <yuriadams@gmail.com>
2018-06-07 11:49:38 -0300

    get consul config function and container tags function

--- src/com/cvc/Pipeline.groovy
+++ src/com/cvc/Pipeline.groovy
@@ -1,6 +1,24 @@
 #!/usr/bin/groovy
 package com.cvc;
 
+def consul(config, keyName) {
+  def consulURL = config.consul[env.JOB_BASE_NAME]['url']
+  def consulPrefix = config.consul.prefix
+  def ssh = config.app[env.JOB_BASE_NAME]['sshJenkinsLab']
+
+  def consulData = sh(
+      script: "${ssh} curl -s http://${consulURL}/v1/kv/${consulPrefix}/${keyName} | jq '.[0].Value' | tr -d '\"'",
+      returnStdout: true
+  )
+
+  def decodedData = sh(
+      script: "echo '${consulData}' | base64 --decode",
+      returnStdout: true
+  )
+
+  return decodedData
+}
+
 def notifyBuild(String message, String channel, String baseUrl, String tokenCredentialId, String buildStatus = 'STARTED') {
     buildStatus = buildStatus ?: 'SUCCESSFUL'
     def colorName = 'RED'
@@ -22,4 +40,34 @@ def notifyBuild(String message, String channel, String baseUrl, String tokenCred
               tokenCredentialId: tokenCredentialId,
               color:"${colorCode}",
               message: "${summary} ${message}")
+}
+
+def getContainerTags(config, Map tags = [:]) {
+    println "getting list of tags for container"
+    def String commit_tag
+    def String version_tag
+
+    // commit tag
+    try {
+        // if branch available, use as prefix, otherwise only commit hash
+        if (env.BRANCH_NAME) {
+            commit_tag = env.BRANCH_NAME + '-' + env.GIT_COMMIT_ID.substring(0, 7)
+        } else {
+            commit_tag = env.GIT_COMMIT_ID.substring(0, 7)
+        }
+        tags << ['commit': commit_tag]
+    } catch (Exception e) {
+        println "WARNING: commit unavailable from env. ${e}"
+    }
+
+    // build tag only if none of the above are available
+    if (!tags) {
+        try {
+            tags << ['build': env.BUILD_TAG]
+        } catch (Exception e) {
+            println "WARNING: build tag unavailable from config.project. ${e}"
+        }
+    }
+
+    return tags
 }
