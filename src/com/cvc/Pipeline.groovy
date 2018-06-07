a75cfcf562b347f26f247697a25726b0d7469ddd
yuriadams <yuriadams@gmail.com>
2018-06-06 21:27:55 -0300

    notify build method

--- src/com/cvc/Pipeline.groovy
+++ src/com/cvc/Pipeline.groovy
@@ -1,8 +1,25 @@
 #!/usr/bin/groovy
 package com.cvc;
 
-def kubectlTest() {
-  // Test that kubectl can correctly communication with the Kubernetes API
-  println "checking kubectl connnectivity to the API"
-  sh "kubectl get nodes"
+def notifyBuild(String message, String channel, String baseUrl, String tokenCredentialId, String buildStatus = 'STARTED') {
+    buildStatus = buildStatus ?: 'SUCCESSFUL'
+    def colorName = 'RED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} (${env.JOB_URL})"
+
+    // Override default values based on build status
+    if (buildStatus == 'STARTED') {
+        colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESSFUL' || buildStatus == 'SUCCESS') {
+        colorCode = '#00FF00'
+    } else {
+        colorCode = '#FF0000'
+    }
+
+    slackSend(channel: channel,
+              baseUrl: baseUrl,
+              tokenCredentialId: tokenCredentialId,
+              color:"${colorCode}",
+              message: "${summary} ${message}")
 }
